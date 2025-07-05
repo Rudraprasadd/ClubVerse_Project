e04d1ffbe28b61c2e17679be81199ab5abc64a24
Rudraprasad Satapathy <rudraprasadsatapathy3506@gmail.com>
2025-07-05 15:30:59 +0530

    Initial commit

---
+++ src/main/java/com/clubverse/ClubVerse/ClubVerseApplication.java
@@ -0,0 +1,13 @@
+package com.clubverse.ClubVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clubverse/ClubVerse/ClubVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubverse.ClubVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
